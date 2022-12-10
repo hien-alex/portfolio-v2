5d8b473dcb5dde7469fa4d76e7442cb27b24ce28
hien-alex <85124411+hien-alex@users.noreply.github.com>
2022-12-10 13:45:12 -0800

    updated images, added to about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,5 +1,15 @@
 import NonInteractiveCanvas from "../NonInteractiveCanvas/NonInteractiveCanvas";
 import avatar from "../../Assets/user--avatar.svg";
+import cssIcon from "../../Assets/Images/css.png";
+import expressIcon from "../../Assets/Images/express.png";
+import gitIcon from "../../Assets/Images/git.png";
+import htmlIcon from "../../Assets/Images/html.png";
+import javascriptIcon from "../../Assets/Images/javascript.png";
+import mongoDBIcon from "../../Assets/Images/mongodb.png";
+import mySQLIcon from "../../Assets/Images/mysql.png";
+import nodeIcon from "../../Assets/Images/node.png";
+import reactIcon from "../../Assets/Images/react.png";
+import sassIcon from "../../Assets/Images/sass.png";
 
 const About = () => {
   return (
@@ -8,7 +18,7 @@ const About = () => {
       <div id={"about-contents"}>
         <h1 id={"about-header"}>About</h1>
         <div id={"about-info"}>
-          <div id={"about-left"}>
+          <div id={"about-intro"}>
             <img id={"about-avatar"} src={avatar}></img>
             <div>
               Hi I'm Alex! At this moment, I'm looking for entry / junior level
@@ -29,22 +39,58 @@ const About = () => {
               developer!
             </div>
           </div>
-          <div id={"about-right"}>
-            Hi I'm Alex! At this moment, I'm looking for entry / junior level
-            oppurtunities in software development. I'm a recent graduate of
-            BCITs Computer Science program, majoring in Computer Systems
-            Technology. Much of my education was composed of software
-            development courses such as object oriented programming, procedural
-            programming, algorithms, and data structures. In those courses, I've
-            gained a thorough understanding of languages such as Python, Java,
-            and Javascript. However, I do have some experience in webdevelopment
-            and databases, utilizing React, SQL, and R. As of now, my main focus
-            is on expanding my abilities to apply algorithms and data structures
-            to practical situations. I have created small projects utilizing web
-            development with common algorithms but programming in Python is what
-            I'm most familiar with. At this moment I'm excited to apply what
-            I've learned and to find opportunities will teach me what it takes
-            to be a software developer!
+          <div id={"about-skills"}>
+            <div className="skills-column">
+              <div className="skill-container">
+                <img className="skill-icon-container" src={cssIcon}></img>
+                <div className="skill-name">CSS</div>
+              </div>
+              <div className="skill-container">
+                <img className="skill-icon-container" src={expressIcon}></img>
+                <div className="skill-name">EXPRESS.JS</div>
+              </div>
+              <div className="skill-container">
+                <img className="skill-icon-container" src={gitIcon}></img>
+                <div className="skill-name">GIT</div>
+              </div>
+            </div>
+
+            <div className="skills-column">
+              <div className="skill-container">
+                <img className="skill-icon-container" src={htmlIcon}></img>
+                <div className="skill-name">HTML</div>
+              </div>
+              <div className="skill-container">
+                <img
+                  className="skill-icon-container"
+                  src={javascriptIcon}
+                ></img>
+                <div className="skill-name">JAVASCRIPT</div>
+              </div>
+              <div className="skill-container">
+                <img className="skill-icon-container" src={mongoDBIcon}></img>
+                <div className="skill-name">MONGODB</div>
+              </div>
+              <div className="skill-container">
+                <img className="skill-icon-container" src={mySQLIcon}></img>
+                <div className="skill-name">MYSQL</div>
+              </div>
+            </div>
+
+            <div className="skills-column">
+              <div className="skill-container">
+                <img className="skill-icon-container" src={nodeIcon}></img>
+                <div className="skill-name">NODE.JS</div>
+              </div>
+              <div className="skill-container">
+                <img className="skill-icon-container" src={reactIcon}></img>
+                <div className="skill-name">REACT</div>
+              </div>
+              <div className="skill-container">
+                <img className="skill-icon-container" src={sassIcon}></img>
+                <div className="skill-name">SASS</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
