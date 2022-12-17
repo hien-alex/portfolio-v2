935d5b06bae5e599fe479b779001ed767f5fe226
hien-alex <85124411+hien-alex@users.noreply.github.com>
2022-12-17 14:43:57 -0800

    skeleton contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from "./Pages/Landing/Landing.js";
 import Navbar from "./Pages/Navbar/Navbar";
 import About from "./Pages/About/About";
 import Projects from "./Pages/Projects/Projects";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,23 @@
+const Contact = () => {
+  return (
+    <div id={"contact-container"}>
+      <h1 id={"contact-header"}>Contact</h1>
+      <div id={"contact-content"}>
+        <span id={"contact-text"}>
+          Have inquiries or want to work together? Leave your details and I'll
+          get back to you as soon as possible.
+        </span>
+        <form id={"contact-form"}>
+          <input className={"contact-form-input"} placeholder="Name" />
+          <input className={"contact-form-input"} placeholder="Email" />
+          <textarea className={"contact-form-input"} placeholder="Message" />
+          <div id={"contact-form-footer"}>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
